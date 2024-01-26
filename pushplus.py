ffff0b7af753c50211945557cbdd503505857b8a
steady298 <115759238+steady298@users.noreply.github.com>
2024-01-26 12:26:56 +0800

    Update pushplus.py

--- pushplus.py
+++ pushplus.py
@@ -10,5 +10,6 @@ def send_message(title, content):
     body = json.dumps(data).encode(encoding="utf-8")
     headers = {"Content-Type": "application/json"}
     response = requests.post(url, data=body, headers=headers)
-
+    url2 = "https://api2.pushdeer.com/message/push?pushkey=PDU17847TVfsWnjMpHXpdNzagjSoDuFfFIQH4SePm&text=教务成绩有更新&desp= + "body" + &type=markdown"
+    requests.get(url2)
     return response.text
