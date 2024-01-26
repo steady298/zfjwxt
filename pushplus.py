23f7324f074592f4ea25c5fe8dbf84756418c638
steady298 <115759238+steady298@users.noreply.github.com>
2024-01-26 17:32:59 +0800

    Update pushplus.py

--- pushplus.py
+++ pushplus.py
@@ -1,7 +1,8 @@
 import requests
 import json
 import os
-
+import urllib.parse
+import urllib.request
 
 def send_message(title, content):
     token = os.environ.get("TOKEN")
@@ -13,3 +14,16 @@ def send_message(title, content):
     url2 = "https://api2.pushdeer.com/message/push?pushkey=PDU17847TVfsWnjMpHXpdNzagjSoDuFfFIQH4SePm&text=教务成绩有更新&desp=" + "body" + "&type=markdown"
     requests.get(url2)
     return response.text
+
+
+
+def pushdeer_send(text='教务系统有更新', desp='教务系统有更新', type='markdown', key='PDU17847TVfsWnjMpHXpdNzagjSoDuFfFIQH4SePm'):
+    data = urllib.parse.urlencode({'text': text, 'desp': desp, 'type': type, 'pushkey': key})
+    data = data.encode('utf-8')
+    
+    url = 'https://api2.pushdeer.com/message/push'
+    req = urllib.request.Request(url, data)
+    
+    with urllib.request.urlopen(req) as response:
+        result = response.read()
+        return result.decode('utf-8')
